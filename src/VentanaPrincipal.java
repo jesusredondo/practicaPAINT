96cb0c5b4630593742545c74f278cea665889b2c
pherrerop01 <pherrerop01@informatica.iesvalledeljerteplasencia.es>
2016-11-21 18:07:25 +0100

    df
    f

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -1,10 +1,14 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
@@ -21,126 +25,131 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
-
 
 public class VentanaPrincipal {
 
 	final int strokeGOMA = 10;
-	
+
 	/**
 	 * IMPORTANTE: CADA HERRAMIENTA TENDRÃ UN CÃ“DIGO ASOCIADO
 	 */
 	final static int BOLIGRAFO = 0;
 	final static int GOMA = 1;
-	//AÃ‘ADE AQUÃ TU HERRAMIENTA;
-	//TODO: AÃ±adir la herramienta	
-	 
-	//Labels
+	final static int TEXTO = 20;
+	// AÃ‘ADE AQUÃ TU HERRAMIENTA;
+	// TODO: AÃ±adir la herramienta
+
+	//JTextField para introducir el texto
+	JTextField jtextFieldTexto;
+	
+	// Labels
 	JLabel labelTipoLetra;
 	JLabel labelEstiloLetra;
 	JLabel labelTamanioLetra;
+
+	// Listas
+	JComboBox comboBoxTipoLetra;
+	JComboBox comboBoxFuenteLetra;
+	JComboBox comboBoxTamanioLetra;
 	
+	//variables dinamicas, escuchas de los comboBox
+	String tipoLetraComboBox;
+	String fuenteLetraComboBox;
+	String tamanioLetraComboBox;
+
+	//Relleno ComboBox estilo de letra 0,1,2
+	String[]  estiloLetra = {"Normal","Negrita","Curisva"};
 	
-	//Listas
-	JComboBox<String> listaTipoLetra;
-	JComboBox<String> listaEstiloLetra;
-	JComboBox<String> listaTamanioLetra;
-	
-	
-	int herramientaActual = -1; //No hay nada por defecto.	
-	
-	//La ventana principal, en este caso, guarda todos los componentes:
+	// Relleno ComoboBox tamaño de letra
+	String[] tamanioLetra;
+
+	int herramientaActual = -1; // No hay nada por defecto.
+
+	// La ventana principal, en este caso, guarda todos los componentes:
 	JFrame ventana;
-	
-	//Paneles:
+
+	// Paneles:
 	JPanel panelSuperior;
 	JPanel panelInferior;
 	JPanel panelTexto;
-	
-	
-	//Variables para dibujo
+
+	// Variables para dibujo
 	JLabel lienzo;
 	BufferedImage canvas;
-	
-	//Selector de colores;
+
+	// Selector de colores;
 	SelectorColor selector1;
 	SelectorColor selector2;
-	
-	//Botones:
+
+	// Botones:
 	JButton botonNuevo;
 	JButton botonBoligrafo;
 	JButton botonGoma;
 	JButton botonTexto;
-	
-	//VARIABLES PROPIAS DE CADA GRUPO:
-	//Grupo JesÃºs:
+
+	// VARIABLES PROPIAS DE CADA GRUPO:
+	// Grupo JesÃºs:
 	int xAnt;
 	int yAnt;
-	
-	
-	//Constructor, marca el tamaÃ±o y el cierre del frame
+
+	// Constructor, marca el tamaÃ±o y el cierre del frame
 	public VentanaPrincipal() {
 		ventana = new JFrame();
 		ventana.setBounds(100, 50, 800, 600);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 	/**
 	 * MÃ©todo que inicializa todos los componentes de la ventana
 	 */
-	public void inicializarComponentes(){
-		
+	public void inicializarComponentes() {
+
 		ventana.setLayout(new GridBagLayout());
-		
-		//************************************************
-		//PANEL SUPERIOR Y COMPONENTES DE PANEL SUPERIOR
-		//************************************************
+
+		// ************************************************
+		// PANEL SUPERIOR Y COMPONENTES DE PANEL SUPERIOR
+		// ************************************************
 		panelSuperior = new JPanel();
 		panelSuperior.setLayout(new GridBagLayout());
-		panelSuperior.setBorder(BorderFactory.createTitledBorder(
-				BorderFactory.createLineBorder(Color.DARK_GRAY), 
-				"Herramientas", 
-				TitledBorder.CENTER, 
-				TitledBorder.TOP));
-		
-		
+		panelSuperior.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.DARK_GRAY),
+				"Herramientas", TitledBorder.CENTER, TitledBorder.TOP));
+
 		GridBagConstraints settings;
 		settings = new GridBagConstraints();
-		
+
 		settings.gridx = 0;
 		settings.gridy = 0;
 		settings.weightx = 1;
 		settings.ipady = 10;
 		settings.fill = GridBagConstraints.BOTH;
-		ventana.add(panelSuperior,settings);
-		
-		
-		//panel inferior
-		
+		ventana.add(panelSuperior, settings);
+
+		// panel inferior
+
 		panelInferior = new JPanel(new GridBagLayout());
-		
+
 		settings = new GridBagConstraints();
-		
+
 		settings.gridx = 0;
 		settings.gridy = 1;
 		settings.weightx = 1;
 		settings.weighty = 1;
 		settings.fill = GridBagConstraints.BOTH;
 		ventana.add(panelInferior, settings);
-		
-		//BotÃ³n nuevo
+
+		// BotÃ³n nuevo
 		botonNuevo = new JButton(cargarIconoBoton("Imagenes/nuevo.png"));
 		settings = new GridBagConstraints();
 		settings.gridx = 0;
 		settings.gridy = 0;
 		settings.insets = new Insets(0, 10, 0, 0);
 		panelSuperior.add(botonNuevo, settings);
-		
-		//Selector de color1
+
+		// Selector de color1
 		selector1 = new SelectorColor(Color.ORANGE);
 		settings = new GridBagConstraints();
 		settings.gridx = 1;
@@ -148,9 +157,8 @@ public class VentanaPrincipal {
 		settings.insets = new Insets(0, 10, 0, 0);
 		settings.fill = GridBagConstraints.BOTH;
 		panelSuperior.add(selector1, settings);
-		
-		
-		//Selector de color2
+
+		// Selector de color2
 		selector2 = new SelectorColor(Color.WHITE);
 		settings = new GridBagConstraints();
 		settings.gridx = 2;
@@ -158,47 +166,47 @@ public class VentanaPrincipal {
 		settings.insets = new Insets(0, 10, 0, 0);
 		settings.fill = GridBagConstraints.BOTH;
 		panelSuperior.add(selector2, settings);
-		
-		
-		//Herramienta de bolÃ­grafo
+
+		// Herramienta de bolÃ­grafo
 		botonBoligrafo = new JButton(cargarIconoBoton("Imagenes/boligrafo.png"));
 		settings = new GridBagConstraints();
 		settings.gridx = 3;
 		settings.gridy = 0;
 		settings.insets = new Insets(0, 10, 0, 0);
 		panelSuperior.add(botonBoligrafo, settings);
-		
-		//Herramienta de borrar
+
+		// Herramienta de borrar
 		botonGoma = new JButton(cargarIconoBoton("Imagenes/borrar.png"));
 		settings = new GridBagConstraints();
 		settings.gridx = 4;
 		settings.gridy = 0;
 		settings.insets = new Insets(0, 10, 0, 0);
 		panelSuperior.add(botonGoma, settings);
-		
+
 		/**
 		 * VUESTRAS HERRAMIENTAS AQUÃ�
 		 */
-		//TODO: Insertar un botÃ³n e implementar mi herramienta.
-		
-	
-		
-		//Un elemento que ocupe todo el espacio a la derecha:
+		// TODO: Insertar un botÃ³n e implementar mi herramienta.
+
+		// Un elemento que ocupe todo el espacio a la derecha:
 		JPanel panelEspacioDerecha = new JPanel();
 		settings = new GridBagConstraints();
 		settings.gridx = 6; /*** OJO ***/
 		settings.gridy = 0;
 		settings.weightx = 1;
 		panelSuperior.add(panelEspacioDerecha, settings);
-		
-		//*******************************************************************************
-		//PABLO Y RUBEN
-		//panel de texto y componente
+
+		// *******************************************************************************
+		// PABLO Y RUBEN
+		// panel de texto y componente
 		panelTexto = new JPanel();
 		panelTexto.setLayout(new GridBagLayout());
-		panelTexto.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+		panelTexto.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+
+		//Jtextfield para el texto
+		jtextFieldTexto = new JTextField();
 		
-		//Herramienta para crear texto
+		// Herramienta para crear texto
 		botonTexto = new JButton(cargarIconoBoton("Imagenes/texto.png"));
 		settings = new GridBagConstraints();
 		settings.gridx = 0;
@@ -206,9 +214,9 @@ public class VentanaPrincipal {
 		settings.gridheight = 2;
 		settings.fill = GridBagConstraints.BOTH;
 		settings.insets = new Insets(1, 1, 1, 1);
-		panelTexto.add(botonTexto, settings);		
-		
-		//labels del componente
+		panelTexto.add(botonTexto, settings);
+
+		// labels del componente
 		labelEstiloLetra = new JLabel("Fuente");
 		settings = new GridBagConstraints();
 		settings.gridx = 1;
@@ -216,60 +224,66 @@ public class VentanaPrincipal {
 		settings.gridheight = 1;
 		settings.fill = GridBagConstraints.BOTH;
 		settings.insets = new Insets(1, 1, 1, 1);
-		panelTexto.add(labelEstiloLetra, settings);	
-		
+		panelTexto.add(labelEstiloLetra, settings);
+
 		labelTamanioLetra = new JLabel("Tamaño");
 		settings = new GridBagConstraints();
 		settings.gridx = 2;
 		settings.gridy = 0;
 		settings.gridheight = 1;
 		settings.insets = new Insets(1, 1, 1, 1);
-		panelTexto.add(labelTamanioLetra, settings);	
-		
+		panelTexto.add(labelTamanioLetra, settings);
+
 		labelTipoLetra = new JLabel("Tipo");
 		settings = new GridBagConstraints();
 		settings.gridx = 3;
 		settings.gridy = 0;
 		settings.gridheight = 1;
 		settings.insets = new Insets(1, 1, 1, 1);
-		panelTexto.add(labelTipoLetra, settings);	
-		
-		//jComboBox del componente
-		listaEstiloLetra = new JComboBox();
+		panelTexto.add(labelTipoLetra, settings);
+
+		// jComboBox del componente
+		comboBoxFuenteLetra = new JComboBox(fuetesLetras());
 		settings = new GridBagConstraints();
 		settings.gridx = 1;
 		settings.gridy = 1;
 		settings.gridheight = 1;
 		settings.insets = new Insets(1, 1, 1, 1);
-		panelTexto.add(listaEstiloLetra, settings);	
-		
-		listaTamanioLetra = new JComboBox();
+		panelTexto.add(comboBoxFuenteLetra, settings);
+
+		tamanioLetra = new String[100];
+		for (int i = 0; i < 100; i++) {
+				tamanioLetra[i] = "" + (i+1);
+			
+		}
+		comboBoxTamanioLetra = new JComboBox(tamanioLetra);
 		settings = new GridBagConstraints();
 		settings.gridx = 2;
 		settings.gridy = 1;
 		settings.gridheight = 1;
 		settings.insets = new Insets(1, 1, 1, 1);
-		panelTexto.add(listaTamanioLetra, settings);	
-		
-		listaTipoLetra = new JComboBox();
+		panelTexto.add(comboBoxTamanioLetra, settings);
+
+		comboBoxTipoLetra = new JComboBox(estiloLetra);
 		settings = new GridBagConstraints();
 		settings.gridx = 3;
 		settings.gridy = 1;
 		settings.gridheight = 1;
 		settings.insets = new Insets(1, 1, 1, 1);
-		panelTexto.add(listaTipoLetra, settings);	
-		
+		panelTexto.add(comboBoxTipoLetra, settings);
+
 		settings = new GridBagConstraints();
 		settings.gridx = 5;
 		settings.gridy = 0;
 		settings.insets = new Insets(0, 10, 0, 0);
 		panelSuperior.add(panelTexto, settings);
-		
-		//**************************************************************************FIN PABLO Y RUBEN
-		
-		//***************************
-		//EL LIENZO DONDE PINTAMOS. 
-		//***************************
+
+		// **************************************************************************FIN
+		// PABLO Y RUBEN
+
+		// ***************************
+		// EL LIENZO DONDE PINTAMOS.
+		// ***************************
 		lienzo = new JLabel();
 		lienzo.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		lienzo.setHorizontalAlignment(SwingConstants.CENTER);
@@ -278,87 +292,92 @@ public class VentanaPrincipal {
 		settings.gridy = 0;
 		panelInferior.add(lienzo, settings);
 		ventana.repaint();
-		
-	}
-	
 
+	}
 
 	/**
 	 * MÃ©todo que inicializa todos los listeners del programa.
 	 */
-	public void inicializarListeners(){
+	public void inicializarListeners() {
 		
-		//LÃ­stener de carga de VentanaPrincipal. Cuando se carga la pantalla es cuando se puede inicializar el canvas.
+
+		// LÃ­stener de carga de VentanaPrincipal. Cuando se carga la pantalla
+		// es cuando se puede inicializar el canvas.
 		ventana.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowOpened(WindowEvent e) {
 				borrarCanvas();
 			}
 		});
-		
+
 		botonNuevo.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				borrarCanvas();
 			}
 		});
-		
-		
-		
+
 		/**
 		 * Cada nueva herramienta que aÃ±adas, tendrÃ¡ un nuevo lÃ­stener:
 		 */
 		botonBoligrafo.addActionListener(anadirListenerHerramienta(BOLIGRAFO));
 		botonGoma.addActionListener(anadirListenerHerramienta(GOMA));
-		//TODO: AÃ±adir nuevos listeners para las herramientas:
-		
-		
-		
-		
+		// TODO: AÃ±adir nuevos listeners para las herramientas:
+		botonTexto.addActionListener(anadirListenerHerramienta(TEXTO));
+
 		lienzo.addMouseListener(new MouseAdapter() {
 			
 			@Override
+			public void mouseClicked(MouseEvent e) {
+				// Dependiendo de la herramienta...
+				switch (herramientaActual) {
+				case TEXTO:
+					incluirCampotexto(e);
+					break;
+				default:
+					break;
+				}
+			}
+
+			@Override
 			public void mousePressed(MouseEvent e) {
-				//Dependiendo de la herramienta...
+				// Dependiendo de la herramienta...
 				switch (herramientaActual) {
 				case BOLIGRAFO:
 					mousePressedBoligrafo(e);
 					break;
-
 				case GOMA:
 					borraGoma(e);
 				default:
 					break;
-				}	
+				}
 			}
-			
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				//Dependiendo de la herramienta...
+				// Dependiendo de la herramienta...
 				switch (herramientaActual) {
 				case BOLIGRAFO:
-					mouseDraggedBoligrafo(e); //Me vale este mÃ©todo
+					mouseDraggedBoligrafo(e); // Me vale este mÃ©todo
 					break;
 
 				case GOMA:
 					borraGoma(e);
 					break;
-					
 				default:
 					break;
-				}	
+				}
 				/** OJO **/
 				lienzo.repaint();
 			}
-			
+
 		});
-		
-		
+
 		lienzo.addMouseMotionListener(new MouseMotionAdapter() {
-			
+
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				//Dependiendo de la herramienta...
+				// Dependiendo de la herramienta...
 				switch (herramientaActual) {
 				case BOLIGRAFO:
 					mouseDraggedBoligrafo(e);
@@ -367,43 +386,42 @@ public class VentanaPrincipal {
 				case GOMA:
 					borraGoma(e);
 					break;
-					
 				default:
 					break;
-				}				
+				}
 				/** OJO **/
 				lienzo.repaint();
 			}
-			
+
 		});
-		
-		
+
 	}
-	
-	
+
 	/**
-	 * MÃ©todo que Borra el canvas para pintarlo completamente en Blanco.
-	 * El nuevo canvas se adapta al tamanio del lienzo.
+	 * MÃ©todo que Borra el canvas para pintarlo completamente en Blanco. El
+	 * nuevo canvas se adapta al tamanio del lienzo.
 	 */
-	public void borrarCanvas(){
+	public void borrarCanvas() {
 		canvas = new BufferedImage(panelInferior.getWidth(), panelInferior.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		lienzo.setIcon(new ImageIcon(canvas));
-		
+
 		Graphics graficos = canvas.getGraphics();
 		graficos.setColor(selector2.getColor());
 		graficos.fillRect(0, 0, panelInferior.getWidth(), panelInferior.getHeight());
 		graficos.dispose();
 		lienzo.repaint();
 	}
-	
-	
+
 	/**
 	 * MÃ©todo que nos devuelve un icono para la barra de herramientas superior.
-	 * NOTA: SerÃ­a conveniente colocar una imagen con fondo transparente y que sea cuadrada, para no estropear la interfaz.
-	 * @param rutaImagen: La ruta de la imagen.
+	 * NOTA: SerÃ­a conveniente colocar una imagen con fondo transparente y que
+	 * sea cuadrada, para no estropear la interfaz.
+	 * 
+	 * @param rutaImagen:
+	 *            La ruta de la imagen.
 	 * @return El ImageIcon que se utilizarÃ¡ en un botÃ³n.
 	 */
-	public ImageIcon cargarIconoBoton(String rutaImagen){
+	public ImageIcon cargarIconoBoton(String rutaImagen) {
 		BufferedImage bufferAuxiliar = null;
 		try {
 			bufferAuxiliar = ImageIO.read(new File(rutaImagen));
@@ -412,14 +430,16 @@ public class VentanaPrincipal {
 		}
 		return new ImageIcon(bufferAuxiliar.getScaledInstance(40, 40, BufferedImage.SCALE_SMOOTH));
 	}
-	
-	
+
 	/**
-	 * MÃ©todo que devuelve un actionListener que cambia la herramienta Actual a la que se pasa por parÃ¡metros
+	 * MÃ©todo que devuelve un actionListener que cambia la herramienta Actual a
+	 * la que se pasa por parÃ¡metros
+	 * 
 	 * @param herramienta
-	 * @return Un action listener que cambia la herramienta actual. Se puede utilizar sobre los botones, por ejemplo.
+	 * @return Un action listener que cambia la herramienta actual. Se puede
+	 *         utilizar sobre los botones, por ejemplo.
 	 */
-	public ActionListener anadirListenerHerramienta(int herramienta){
+	public ActionListener anadirListenerHerramienta(int herramienta) {
 		return new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -427,65 +447,131 @@ public class VentanaPrincipal {
 			}
 		};
 	}
-	
-	
+
 	/**
-	 * MÃ©todo que realiza todas las llamadas necesarias para inicializar la ventana correctamente.
+	 * MÃ©todo que realiza todas las llamadas necesarias para inicializar la
+	 * ventana correctamente.
 	 */
-	public void inicializar(){
+	public void inicializar() {
 		ventana.setVisible(true);
-		inicializarComponentes();	
-		inicializarListeners();		
+		inicializarComponentes();
+		inicializarListeners();
 	}
-	
-	
-	
+
 	/*****************************************
 	 *****************************************
 	 * AQUÃ� VAN LOS MÃ‰TODOS DE LOS LISTENERS:
 	 *****************************************
 	 *****************************************/
-	
-	
+
 	/**
 	 * Determina xAnt e yAnt para el resto del trazo
+	 * 
 	 * @param e
 	 */
-	private void mousePressedBoligrafo(MouseEvent e){
+	private void mousePressedBoligrafo(MouseEvent e) {
 		xAnt = e.getX();
 		yAnt = e.getY();
 	}
-	
+
 	/**
 	 * Pinta la lÃ­nea del bolÃ­grafo al arrastrar.
+	 * 
 	 * @param e
 	 */
-	private void mouseDraggedBoligrafo(MouseEvent e){
+	private void mouseDraggedBoligrafo(MouseEvent e) {
 		Graphics graficos = canvas.getGraphics();
 		graficos.setColor(selector1.getColor());
 		graficos.drawLine(xAnt, yAnt, e.getX(), e.getY());
 		graficos.dispose();
-		
+
 		xAnt = e.getX();
 		yAnt = e.getY();
 	}
-	
-	
+
 	/**
 	 * Borra donde estÃ© el ratÃ³n.
+	 * 
 	 * @param e
 	 */
-	private void borraGoma(MouseEvent e){
+	private void borraGoma(MouseEvent e) {
 		Graphics graficos = canvas.getGraphics();
 		graficos.setColor(selector2.getColor());
-		graficos.fillOval(e.getX()-(strokeGOMA/2), 
-				e.getY()-(strokeGOMA/2), 
-				strokeGOMA, 
-				strokeGOMA);
+		graficos.fillOval(e.getX() - (strokeGOMA / 2), e.getY() - (strokeGOMA / 2), strokeGOMA, strokeGOMA);
 		graficos.dispose();
 	}
+	private void incluirCampotexto(MouseEvent e){
+		Graphics graficos = canvas.getGraphics();
+		Font fuente;
+		int tipofuente = 0;
 		
-	
-	
-	
+		//por defecto se establece la fuente, tamaño y tipo de letra
+		if (fuenteLetraComboBox == null ) {
+			fuenteLetraComboBox = comboBoxFuenteLetra.getItemAt(0).toString();
+		}
+		if (tamanioLetraComboBox == null) {
+			tamanioLetraComboBox = comboBoxTamanioLetra.getItemAt(0).toString();
+		}
+		if (tipoLetraComboBox == null) {
+			tipoLetraComboBox = ""+0;
+		}
+		//escucho el comboBox de fuente de letra
+		comboBoxFuenteLetra.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				fuenteLetraComboBox = e.getItem().toString();
+				
+			}
+		});
+		//escucho el comboBox del tamanio de letra
+		comboBoxTamanioLetra.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				tamanioLetraComboBox = e.getItem().toString();
+				
+			}
+		});
+		//escucho el comboBox del tipo de letra
+		comboBoxTipoLetra.addItemListener(new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				tipoLetraComboBox = e.getItem().toString();
+				
+			}
+		});
+		//dependiendo del estilo de letra
+		switch (tipoLetraComboBox) {
+		case "Normal":
+			tipofuente = 0;
+			break;
+		case "Negrita":
+			tipofuente = 1;
+			break;
+		case "Cursiva":
+			tipofuente = 2;
+			break;
+		}
+		//creamo una fuente nueva
+		fuente = new Font(fuenteLetraComboBox, tipofuente, Integer.parseInt(tamanioLetraComboBox));
+		jtextFieldTexto.setFont(fuente);
+		jtextFieldTexto.setOpaque(false);
+		
+		
+		
+		
+		
+	}
+
+	private String[] fuetesLetras() {
+
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		String[] fuentes = ge.getAvailableFontFamilyNames();
+		return fuentes;
+		// comboBoxLetra = new JComboBox(fuentes);
+
+	}
+
 }
